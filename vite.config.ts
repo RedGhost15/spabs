4faebd6457d9288fa9c58c9089f402af342099ae
unknown <eduard.tudorache@gmail.com>
2025-04-09 21:12:25 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // Replace with your actual base path
 export default defineConfig({
-  base: '/', // 👈 IMPORTANT if deployed under a subpath like GitHub Pages or Vercel with that domain
+  base: '/spabs/', // 👈 IMPORTANT if deployed under a subpath like GitHub Pages or Vercel with that domain
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
